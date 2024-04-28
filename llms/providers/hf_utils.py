17229d3766fb7eb9894cd7b0e3bb4ee99b372c6d
Vishruth Veerendranath <vveerend@andrew.cmu.edu>
2024-04-28 17:16:38 -0400

    Added HF token for Inference API usage

--- llms/providers/hf_utils.py
+++ llms/providers/hf_utils.py
@@ -1,4 +1,5 @@
 from text_generation import Client  # type: ignore
+import os
 
 
 def generate_from_huggingface_completion(
@@ -9,13 +10,14 @@ def generate_from_huggingface_completion(
     max_new_tokens: int,
     stop_sequences: list[str] | None = None,
 ) -> str:
-    client = Client(model_endpoint, timeout=60)
+    client = Client(model_endpoint, headers = {"Authorization": f"Bearer {os.environ.get('HF_API_KEY')}"}, timeout=60)
     generation: str = client.generate(
         prompt=prompt,
         temperature=temperature,
         top_p=top_p,
         max_new_tokens=max_new_tokens,
         stop_sequences=stop_sequences,
+        truncate=8000 # 8k for llama3
     ).generated_text
 
     return generation
